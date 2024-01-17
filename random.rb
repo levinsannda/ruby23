6e0c7eba4065ac3dfcad9d4d3afb216ab2d144fe
levinsannda <156919429+levinsannda@users.noreply.github.com>
2024-01-18 00:01:41 +0900

    Update random.rb

--- random.rb
+++ random.rb
@@ -1,15 +1,18 @@
 def generate_random_number(min, max)
-  # 生成[min, max]范围内的随机整数
+  # [min, max]範囲内の乱数
   rand(min..max)
 end
 
-# 输入最小值和最大值
+# 入力min&max
 puts "min："
 min_value = gets.chomp.to_i
 
 puts "max："
 max_value = gets.chomp.to_i
 
-# 生成并输出随机数
-random_number = generate_random_number(min_value, max_value)
-puts "random number：#{random_number}"
+#output
+if min > max
+    puts "error!"
+  else
+    random_number = rand(min..max)
+    puts "random number: #{random_number}"
